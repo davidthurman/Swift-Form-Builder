74e9d97a75713ef46122130a4a884beb0cb07c67
David Thurman <davidthurmanwork@gmail.com>
2017-03-19 11:35:06 -0400

    Adding carthage support

---
+++ Swift-Form-Builder/Swift-Form-Builder.h
@@ -0,0 +1,19 @@
+//
+//  Swift-Form-Builder.h
+//  Swift-Form-Builder
+//
+//  Created by David Thurman on 3/19/17.
+//  Copyright © 2017 David Thurman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift-Form-Builder.
+FOUNDATION_EXPORT double Swift_Form_BuilderVersionNumber;
+
+//! Project version string for Swift-Form-Builder.
+FOUNDATION_EXPORT const unsigned char Swift_Form_BuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_Form_Builder/PublicHeader.h>
+
+
